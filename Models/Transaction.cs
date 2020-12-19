5ebc9e7abf9584f472a5716577441af5e2f39b44
Andreas <anfauske@gmail.com>
2020-12-19 21:57:13 +0000

    Added model for transactions

---
+++ Models/Transaction.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace SbankenYnab.Models
+{
+    public class TransactionsList
+    {
+        public int AvailableItems { get; set; }
+        public List<Transaction> Items { get; set; }
+    }
+
+    public class Transaction{
+        public DateTime AccountingDate { get; set; }
+        public double Amount { get; set; }
+        public String Text { get; set; }
+        public bool IsReservation { get; set; }
+        public TransactionDetail TransactionDetail { get; set; }
+    }
+
+    public class TransactionDetail
+    {
+        public String Cid { get; set; }
+        public String AmountDescription { get; set; }
+        public String ReceiverName { get; set; }
+        public String PayerName { get; set; }
+        public String RegistrationDate { get; set; }
+    }
+}
