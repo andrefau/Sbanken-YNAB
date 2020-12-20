eb9a136222f91803b84ed3daba7a04f164f1e759
Andreas <anfauske@gmail.com>
2020-12-20 21:50:59 +0000

    Added budget by name

--- Clients/YNABClient.cs
+++ Clients/YNABClient.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SbankenYnab.Credentials;
+using SbankenYnab.Models;
 
 namespace SbankenYnab.Clients
 {
@@ -28,6 +31,28 @@ namespace SbankenYnab.Clients
                     { "Authorization", "Bearer " + YNABCredentials.AccessToken }
                 }
             };
+        }
+
+        public async Task<Budget> GetBudgetByName(String name)
+        {
+            _logger.LogInformation("Gettin budgets from YNAB...");
+
+            var budgetResponse = await _client.GetAsync($"/v1/budgets");
+
+            if (!budgetResponse.IsSuccessStatusCode) throw new Exception(budgetResponse.ReasonPhrase);
+
+            var budgetResult = await budgetResponse.Content.ReadAsStringAsync();
+            var budgetSummary = JsonConvert.DeserializeObject<BudgetSummaryResponse>(budgetResult);
+
+            _logger.LogInformation($"Found {budgetSummary.Data.Budgets.Count} budgets. Trying to find budget by name \"{name}\"...");
+
+            var budget = budgetSummary.Data.Budgets.Find(b => b.Name == name);
+
+            if (budget == null) throw new ArgumentException($"No budget by name \"{name}\" was found.");
+
+            _logger.LogInformation($"Found \"{name}\"");
+
+            return budget;
         }
     }
 }
