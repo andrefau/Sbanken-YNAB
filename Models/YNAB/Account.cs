82f9de72e4dc99a93284ec39d61c5a9187bc5692
Andreas Lersveen Fauske <anfauske@gmail.com>
2020-12-21 07:38:03 +0000

    Added ynab account model

---
+++ Models/YNAB/Account.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace SbankenYnab.Models.YNAB
+{
+    public class AccountResponse
+    {
+        public AccountList Data { get; set; }
+    }
+
+    public class AccountList
+    {
+        public List<Account> Accounts { get; set; }
+    }
+
+    public class Account
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+    }
+}
